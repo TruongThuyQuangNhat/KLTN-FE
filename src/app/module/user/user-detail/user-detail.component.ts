842d223bb3c2c569a9470a83072e9ae8fb17117f
quang nhat <truongthuyquangnhat1@gmail.com>
2023-05-25 23:47:57 +0700

    up

--- src/app/module/user/user-detail/user-detail.component.ts
+++ src/app/module/user/user-detail/user-detail.component.ts
@@ -17,6 +17,37 @@ export class UserDetailComponent implements OnInit {
   department: any;
   position: any;
   gridString: string;
+
+  Sex: string | null;
+  Address: string | null;
+  Age: string | null;
+  BirthDay: Date | null;
+  DateStartWork: Date | null;
+  ManagerId: string | null;
+  DayOffId: string | null;
+  SalaryId: string | null;
+  BonusId: string | null;
+  AdvanceMoneyId: string | null;
+  CCCDNumber: string | null;
+  CCCDIssueDate: Date | null;
+  CCCDAddress: string | null;
+  BHXHNumber: string | null;
+  BHXHIssueDate: Date | null;
+  BHXHStartDate: Date | null;
+  BHYTNumber: string | null;
+  BHYTIssueDate: Date | null;
+  BHYTAddress: string | null;
+  BHTNNumber: string | null;
+  BHTNIssueDate: Date | null;
+  SLDNumber: string | null;
+  SLDAddress: string | null;
+  SLDIssueDate: Date | null;
+  BankNumber: string | null;
+  BankName: string | null;
+  BankAccountName: string | null;
+  HDLDNumber: string | null;
+  HDLDStartDate: Date | null;
+  HDLDEndDate: Date | null;
   constructor(
     private router: Router, 
     private route: ActivatedRoute,
@@ -46,7 +77,33 @@ export class UserDetailComponent implements OnInit {
       this.userService.getUser(this.id).subscribe(res => {
         if(res){
           this.userService.getUserInfo(this.id).subscribe(res => {
-            console.log(res)
+            if(res){
+              this.Sex = res.sex?res.sex:'';
+              this.Address = res.address?res.address:'';
+              this.Age = res.age?res.age:'';
+              this.BirthDay = res.birthDay == "0001-01-01T00:00:00" ? null: new Date(res.birthDay);
+              this.DateStartWork = res.dateStartWork == "0001-01-01T00:00:00" ? null: new Date(res.dateStartWork);
+              this.CCCDNumber = res.cccdNumber?res.cccdNumber:'';
+              this.CCCDIssueDate = res.cccdIssueDate == "0001-01-01T00:00:00" ? null: new Date(res.cccdIssueDate);
+              this.CCCDAddress = res.cccdAddress?res.cccdAddress:'';
+              this.BHXHNumber = res.bhxhNumber?res.bhxhNumber:'';
+              this.BHXHIssueDate = res.bhxhIssueDate == "0001-01-01T00:00:00" ? null: new Date(res.bhxhIssueDate);
+              this.BHXHStartDate = res.bhxhStartDate == "0001-01-01T00:00:00" ? null: new Date(res.bhxhStartDate);
+              this.BHYTNumber = res.bhytNumber?res.bhytNumber:'';
+              this.BHYTIssueDate = res.bhytIssueDate == "0001-01-01T00:00:00" ? null: new Date(res.bhytIssueDate);
+              this.BHYTAddress = res.bhytAddress?res.bhytAddress:'';
+              this.BHTNNumber = res.bhtnNumber?res.bhtnNumber:'';
+              this.BHTNIssueDate = res.bhtnIssueDate == "0001-01-01T00:00:00" ? null: new Date(res.bhtnIssueDate);
+              this.SLDNumber = res.sldNumber?res.sldNumber:'';
+              this.SLDAddress = res.sldAddress?res.sldAddress:'';
+              this.SLDIssueDate = res.sldIssueDate == "0001-01-01T00:00:00" ? null: new Date(res.sldIssueDate);
+              this.BankNumber = res.bankNumber?res.bankNumber:'';
+              this.BankName = res.bankName?res.bankName:'';
+              this.BankAccountName = res.bankAccountName?res.bankAccountName:'';
+              this.HDLDNumber = res.hdldNumber?res.hdldNumber:'';
+              this.HDLDStartDate = res.hdldStartDate == "0001-01-01T00:00:00" ? null: new Date(res.hdldStartDate);
+              this.HDLDEndDate = res.hdldEndDate == "0001-01-01T00:00:00" ? null: new Date(res.hdldEndDate);
+            }
           })
           this.userService.getOneDepartment(res.departmentId).subscribe(res => {
             if(res){
@@ -62,6 +119,38 @@ export class UserDetailComponent implements OnInit {
         }
       })
     }
+  }
+
+  saveInfo(){
+    console.log(
+      {
+        Sex: this.Sex,
+        Address: this.Address,
+        Age: this.Age,
+        BirthDay: this.BirthDay?.toISOString(),
+        DateStartWork: this.DateStartWork,
+        CCCDNumber: this.CCCDNumber,
+        CCCDIssueDate: this.CCCDIssueDate,
+        CCCDAddress: this.CCCDAddress,
+        BHXHNumber: this.BHXHNumber,
+        BHXHIssueDate: this.BHXHIssueDate,
+        BHXHStartDate: this.BHXHStartDate,
+        BHYTNumber: this.BHYTNumber,
+        BHYTIssueDate: this.BHYTIssueDate,
+        BHYTAddress: this.BHYTAddress,
+        BHTNNumber: this.BHTNNumber,
+        BHTNIssueDate: this.BHTNIssueDate,
+        SLDNumber: this.SLDNumber,
+        SLDAddress: this.SLDAddress,
+        SLDIssueDate: this.SLDIssueDate,
+        BankNumber: this.BankNumber,
+        BankName: this.BankName,
+        BankAccountName: this.BankAccountName,
+        HDLDNumber: this.HDLDNumber,
+        HDLDStartDate: this.HDLDStartDate,
+        HDLDEndDate: this.HDLDEndDate,
+      }
+    )
   }
 
   back(){
